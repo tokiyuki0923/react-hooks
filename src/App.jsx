5200561dda8c315519e39fbe37125a0be9cbebc1
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-05-22 20:55:02 +0900

    useContextはグローバルにデータを保持するためのメソッドみたいなもの。普通はpropsによってデータの受け渡しを行うが、モジュールが増えてくるとデータの受け渡しの管理がめんどくさくなる。よってグローバルによく使うデータを書いておけば、そこから直接都度importすればいいということになる

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useContext} from 'react';
+import Pikachu from './main';
 
 function App() {
   const[count, setCount] =useState(0);
+  const poke = useContext(Pikachu);
 
   const handleClick =()=>{
     setCount(count +1);
@@ -12,6 +14,10 @@ function App() {
       <h1>useState</h1>
       <button onClick={handleClick}>+</button>
       <p>{count}</p>
+      <hr />
+      <h1>useContext</h1>
+      <p>{poke.name}</p>
+      <p>{poke.age}</p>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const poke = {
+  name:"pikachu",
+  age:25,
+}
+
+const Pikachu = createContext(poke);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Pikachu.Provider value={poke}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Pikachu.Provider>
 )
+
+export default Pikachu;
