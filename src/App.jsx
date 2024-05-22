fe104e727ce5a8f978dfadfd089ec40a46c4e3ca
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-05-22 16:12:44 +0900

    useStateのおさらい。状態を管理するよという宣言をする。その後、配列の二つ目の方を更新後の状態として定義する。あとはどういうふうに使うかによって変わる

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[count, setCount] =useState(0);
 
+  const handleClick =()=>{
+    setCount(count +1);
+  }
   return (
-    <>
-    </>
+    <div className="App">
+      <h1>useState</h1>
+      <button onClick={handleClick}>+</button>
+      <p>{count}</p>
+    </div>
   )
 }
 
 export default App
+
